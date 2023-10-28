94a572f6ce592b302b5ff81f776c06db396e82ba
srushtivjm <127848798+srushtivjm@users.noreply.github.com>
2023-10-27 21:53:24 -0400

    Add files via upload

---
+++ Assignment1.java
@@ -0,0 +1,59 @@
+package lectureCOSC1046;
+
+public class Assignment1 {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		while (true) {
+
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+
+			System.out.println("                        ==============");
+			System.out.println("                        I           ");
+			System.out.println("                 IIIIIIIIIIIIIII       ");
+			System.out.println("                 I             II    ");
+			System.out.println(" IIIIIIIIIIIIIIIII          [] III        ");
+			System.out.println(" I               I             IIII");
+			System.out.println(" I               IIIIIIIIIIIIIIIIIII      ");
+			System.out.println("                        I    I              ");
+			System.out.println("                      |--------|         ");
+			System.out.println("                  Awais Helicopter!              ");
+
+			Thread.sleep(100);
+
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+
+			System.out.println("           ==============         ");
+			System.out.println("                        I           ");
+			System.out.println("                 IIIIIIIIIIIIIII       ");
+			System.out.println("                 I             II    ");
+			System.out.println(" IIIIIIIIIIIIIIIII          [] III        ");
+			System.out.println(" I               I             IIII");
+			System.out.println(" I               IIIIIIIIIIIIIIIIIII      ");
+			System.out.println("                        I    I              ");
+			System.out.println("                      |--------|         ");
+			System.out.println("                  Awais Helicopter!              ");
+
+			Thread.sleep(100);
+		}
+
+} 
+		
+
+	}
+
+
